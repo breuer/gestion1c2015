e0bf6456fc0b0a4dd9548b32cb3aeb43b88ddc98
damiancipolat <damian_Cipolat@yahoo.com.ar>
2015-06-16 13:59:39 -0300

    Update Script_migracion.sql

--- tp/SQL/Script_migracion.sql
+++ tp/SQL/Script_migracion.sql
@@ -47,6 +47,7 @@ create table NEW_SOLUTION.Login_incorrectos
 (
 	logfalla_id			bigint identity(1,1),
 	logfalla_user		varchar(50),
+	logfalla_user_id	int,	
 	logfala_intento		int,
 	primary key(logfalla_id)	
 )
